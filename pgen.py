33f426298aa3eabdd7b33c777e1a2d4c88f8983b
Tejas <68140019+tejas303525@users.noreply.github.com>
2023-01-30 20:40:18 +0530

    Update pgen.py

--- pgen.py
+++ pgen.py
@@ -6,8 +6,10 @@ n=int(input("Enter the length of password: "))
 d={}
 name=input("Account name: ")
 
-res="".join(random.choice(string.ascii_letters) for i in range(n))
+res="".join(random.choice(string.ascii_letters+"%"+"$"+"#"+"@"+"!"+"&"+"*"+"()") for i in range(n))
 
+#for i in range(n):
+#   res=random.choice(string.ascii_letters)
 
 for i in range(len(res)):
     if res not in d:
