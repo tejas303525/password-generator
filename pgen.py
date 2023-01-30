0c89b510ebbc2cb91fca1335c6400fbbf5c0a276
Tejas <68140019+tejas303525@users.noreply.github.com>
2023-01-30 18:41:27 +0530

    Add files via upload

---
+++ pgen.py
@@ -0,0 +1,16 @@
+import random
+import string
+import json
+#to create a random password generator
+n=int(input("Enter the length of password: "))
+d={}
+name=input("Account name: ")
+
+res="".join(random.choice(string.ascii_letters) for i in range(n))
+
+
+for i in range(len(res)):
+    if res not in d:
+        d[name]=res
+with open("passwordgen.txt","w") as f:
+    f.write(json.dumps(d))
